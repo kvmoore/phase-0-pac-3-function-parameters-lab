c3a7f23fb72fe4e0594770551d8a1ff12777d33c
Kenneth <kvmoore097@gmail.com>
2023-06-07 20:45:03 -0500

    First parameters lab

---
+++ index.js
@@ -0,0 +1,24 @@
+
+
+function introduction(name) {
+    
+    return (`Hi, my name is ${name}.`);
+  }
+
+  introduction("Aki");
+
+  function introductionWithLanguage(name, language = "JavaScript") {
+
+    return(`Hi, my name is ${name} and I am learning to program in ${language}.`);
+  }
+  
+  
+  function introductionWithLanguageOptional(name, language = "JavaScript") {
+
+    return(`Hi, my name is ${name} and I am learning to program in ${language}.`);
+
+  }
+
+
+
+  
